7482306934a64a075aead7505f0a572ab926d921
Riccardo Cafagna <DesktopRicc+RiccardoCafa@users.noreply.github.com>
2020-05-02 09:44:10 -0300

    Project Structure

---
+++ Assets/Data/Scripts/_app.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _app : MonoBehaviour
+{
+    public static _app instance;
+
+    void Awake()
+    {
+        #region SINGLETON
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+    }
+
+    private void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
