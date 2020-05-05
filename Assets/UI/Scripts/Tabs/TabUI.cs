2717eed79c9913cd8651ac40939e6979489a6ecf
Riccardo Cafagna <DesktopRicc+RiccardoCafa@users.noreply.github.com>
2020-05-05 16:50:40 -0300

    Music Box + Tabs + Roboto Font

---
+++ Assets/UI/Scripts/Tabs/TabUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class MacroTab
+{
+    public List<Button> Activators;
+    public GameObject TheTab;
+}
+
+public class TabUI : MonoBehaviour
+{
+    [SerializeField]
+    public List<MacroTab> tabs;
+    
+    void Start()
+    {
+        foreach(MacroTab tab in tabs)
+        {
+            foreach(Button btn in tab.Activators)
+            {
+                btn.onClick.AddListener(() =>
+                {
+                    CloseAllTabs();
+                    tab.TheTab.gameObject.SetActive(true);
+                });
+            }
+        }
+    }
+    
+    void CloseAllTabs()
+    {
+        foreach(MacroTab tab in tabs)
+        {
+            tab.TheTab.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/UI/Scripts/ToggleUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleUI : MonoBehaviour
+{
+    public void Toggle(GameObject ToggleObj)
+    {
+        ToggleObj.SetActive(!ToggleObj.activeInHierarchy);
+    }
+}
