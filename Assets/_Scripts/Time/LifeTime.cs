13e6085e8f35646d4c020d06787a3689ccb33c9d
Riccardo Cafagna <DesktopRicc+RiccardoCafa@users.noreply.github.com>
2020-05-06 18:38:31 -0300

    Time Logic (com pog)

---
+++ Assets/_Scripts/Time/LifeTime.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+public class LifeTime
+{
+    public int Day;
+    public int Month;
+    public int Year;
+    public int Hour;
+    public int Minute;
+    public int Second;
+
+    public LifeTime(int day, int month, int year, int hour, int minute, int second)
+    {
+        Day = day;
+        Month = month;
+        Year = year;
+        Hour = hour;
+        Minute = minute;
+        Second = second;
+    }
+
+    public void CalculateTime()
+    {
+        bool calculated = false;
+        while (!calculated)
+        {
+            calculated = true;
+
+            if (Second >= Timer.MinuteSize)
+            {
+                Minute++;
+                Second = Second - Timer.MinuteSize;
+                calculated = false;
+            }
+
+            if (Minute >= Timer.HourSize)
+            {
+                Hour++;
+                Minute = Minute - Timer.HourSize;
+                calculated = false;
+            }
+
+            if (Hour >= Timer.DaySize)
+            {
+                Day++;
+                Hour = 0;
+                calculated = false;
+            }
+
+            if (Day > Timer.DayPerMonth)
+            {
+                Month++;
+                Day = 1;
+                calculated = false;
+            }
+
+            if (Month >= Timer.MonthPerYear)
+            {
+                Year++;
+                Month = 1;
+                calculated = false;
+            }
+        }
+    }
+
+    public static bool Later(LifeTime time1, LifeTime time2)
+    {
+        DateTime t1 = CreateFromTime(time1);
+        DateTime t2 = CreateFromTime(time2);
+        return t1.CompareTo(t2) > 0;
+    }
+
+    public static bool Earlier(LifeTime time1, LifeTime time2)
+    {
+        DateTime t1 = CreateFromTime(time1);
+        DateTime t2 = CreateFromTime(time2);
+        return t1.CompareTo(t2) < 0;
+    }
+
+    public static bool Equal(LifeTime time1, LifeTime time2)
+    {
+        DateTime t1 = CreateFromTime(time1);
+        DateTime t2 = CreateFromTime(time2);
+        return t1.CompareTo(t2) == 0;
+    }
+
+    public LifeTime AddTime(int day, int month, int year, int hour, int minute, int second)
+    {
+        LifeTime lt = new LifeTime(Day + day, Month + month, Year + year, Hour + hour, Minute + minute, Second + second);
+        lt.CalculateTime();
+        return lt;
+    }
+
+    private static DateTime CreateFromTime(LifeTime time1)
+    {
+        return new DateTime(time1.Year, time1.Month, time1.Day, time1.Hour, time1.Minute, time1.Second);
+    }
+
+    public LifeTime Clone()
+    {
+        return (LifeTime) this.MemberwiseClone();
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0:00}", Day) + "/"
+              + string.Format("{0:00}", Month) + "/" + Year + "  \n"
+              + string.Format("{0:00}", Hour) + ":"
+              + string.Format("{0:00}", Minute);
+    }
+}
---
+++ Assets/_Scripts/core/GameCore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCore : MonoBehaviour
+{
+    public Timer timer;
+
+    public TimeManager timeManager;
+
+    void Awake()
+    {
+        timer = new Timer();
+
+        timeManager.timer = timer;
+        timer.SecondPerReal = 1240;
+        timer.Start();
+    }
+
+    void Update()
+    {
+        
+    }
+}
