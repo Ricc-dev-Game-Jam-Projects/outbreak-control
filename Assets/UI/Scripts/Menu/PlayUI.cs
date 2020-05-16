caa40c5f0ceb984d10117c3e7d147875755e9534
Riccardo Cafagna <DesktopRicc+RiccardoCafa@users.noreply.github.com>
2020-05-16 17:15:02 -0300

    Seed Loader

---
+++ Assets/UI/Scripts/Menu/PlayUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(Button))]
+public class PlayUI : MonoBehaviour
+{
+    public Button playBtn;
+    public TMP_InputField seedInput;
+    public string SceneToLoad;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playBtn = GetComponent<Button>();
+
+        playBtn.onClick.AddListener(() =>
+        {
+            if (seedInput.text.Length > 0)
+            {
+                int.TryParse(seedInput.text, out int seed);
+                Random.InitState(seed);
+            }
+            LevelManager.instance.LoadScene(SceneToLoad);
+        });
+    }
+}
--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -5,6 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
