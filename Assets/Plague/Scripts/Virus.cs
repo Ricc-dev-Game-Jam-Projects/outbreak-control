6c0944bcb8e536bc4189b006847f3e458353bc90
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2020-05-16 23:03:06 -0300

    few nerfs on virus

--- Assets/Plague/Scripts/Virus.cs
+++ Assets/Plague/Scripts/Virus.cs
@@ -8,6 +8,7 @@ public class Virus
     public float Spreading; // Média de de pessoas contagiadas por 1 pessoa infectada (esse valor pode abaixar com as medidas)
     public float SerialRange; // A cada quantos dias são manifestados os sintomas, contabilizando mais infectados
     public float FatalityCase; // Qual a média de fatalidade entre os infectados
+    public float InfectionTime;
 
     public List<Symptom> MySymptoms { get; private set; } // Sintomas do virus
     public List<Transmission> MyTransmissions { get; private set; } //meios de Transmissao do virus
@@ -28,9 +29,11 @@ public class Virus
             { "Transmission", MyTransmissions.ToArray() as Perk[] }
         };
         
-        Spreading = Random.Range(1.4f, 4.0f);
+        Spreading = Random.Range(0.7f, 1.7f);
         SerialRange = Random.Range(1f, 5f);
-        FatalityCase = Random.Range(0.0005f, 0.04f);
+        FatalityCase = Random.Range(0.0005f, 0.03f);
+        InfectionTime = Random.Range(7f, 14f);
+
 
         Debug.Log("Virus Spreading " + Spreading);
         Debug.Log("Virus Serial Range " + SerialRange);
@@ -130,3 +133,24 @@ public class Virus
         MyTransmissions.Add(transmission);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//string a = "nothing"
