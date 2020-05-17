acf5bd1b85787dbbfff877b9a70ff4183e9eaefd
Riccardo Cafagna <DesktopRicc+RiccardoCafa@users.noreply.github.com>
2020-05-16 21:37:43 -0300

    Barra + Total

--- Assets/Map/Scripts/MapUI.cs
+++ Assets/Map/Scripts/MapUI.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Globalization;
 
 public class MapUI : MonoBehaviour
 {
     public RegionPopUp PopUp;
 
     public Button PopulationButton;
+    public TextMeshProUGUI TotalPopulationText;
     public TextMeshPro PopulationSizeText;
     public TextMeshPro InfectedText;
     public GameObject VirusBar;
+    public GameObject PopBar;
 
     private bool showPopulation;
 
@@ -32,11 +35,15 @@ public class MapUI : MonoBehaviour
             if (reg != null && reg.Type != RegionType.Water)
             {
                 PopulationSizeText.text = ((int)(reg.city.Population * 1e3)) + "";
-                //InfectedText.text = ((int)(reg.city.Infected * 1e3)) + "";
+                InfectedText.text = ((int)(reg.city.Infected / City.Person)) + "";
                 VirusBar.transform.localScale =
-                    new Vector3(reg.city.Infected / reg.city.Population,
+                    new Vector3(reg.city.Infected / City.Person / 1000,
                     VirusBar.transform.localScale.y,
                     VirusBar.transform.localScale.z);
+                PopBar.transform.localScale = 
+                    new Vector3(reg.city.Population / City.Person / 1000,
+                    PopBar.transform.localScale.y,
+                    PopBar.transform.localScale.z);
                 PopUp.OpenOn(RegionBehaviour.RegionSelected.transform.position.x,
                     RegionBehaviour.RegionSelected.transform.position.y);
             }
@@ -45,5 +52,7 @@ public class MapUI : MonoBehaviour
                 PopUp.Close();
             }
         }
+
+        TotalPopulationText.text = City.TotalPopulation.ToString("G", NumberFormatInfo.CurrentInfo);
     }
 }
