388b637a178b8133fd97057946591d690ea59609
Riccardo Cafagna <DesktopRicc+RiccardoCafa@users.noreply.github.com>
2020-05-18 01:19:21 -0300

    Menu

---
+++ Assets/_Scripts/ClearData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClearData : MonoBehaviour
+{
+    public Button ClearDataBtn;
+
+    private void Awake()
+    {
+        ClearDataBtn.onClick.AddListener(() =>
+        {
+            PlayerPrefs.DeleteAll();
+            Application.Quit();
+        });
+    }
+}
